4b270a9006aa508eba7f6ba6655991ac80c8cc1c
MEN3EMOO <men3em4@gmail.com>
2023-09-07 16:41:33 -0700

    Done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * Description: program to output with puts
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
