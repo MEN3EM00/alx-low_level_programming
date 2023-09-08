28da379e8bd0f5cffe8dbcd88f0e66576472a74f
MEN3EMOO <men3em4@gmail.com>
2023-09-07 17:05:46 -0700

    Done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entyr point
+ * Description: program to output with printf
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
